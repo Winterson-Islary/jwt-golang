177b0a36d94ed82f4c71f2bb13b229edfad658a4
Winterson <u19cse1028@cit.ac.in>
2024-04-07 20:32:26 +0530

    added test for user service handlers

---
+++ service/user/routes_test.go
@@ -0,0 +1,58 @@
+package user
+
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/Winterson-Islary/jwt-golang.git/types"
+	"github.com/gorilla/mux"
+)
+
+func TestUserServiceHandlers(t *testing.T) {
+	userStore := &mockUserStore{}
+	handler := NewHandler(userStore)
+
+	t.Run("should fail if the user payload is invalid", func(t *testing.T) {
+		payload := types.RegisterUserPayload{
+			FirstName: "user",
+			LastName:  "tester",
+			Email:     "",
+			Password:  "123",
+		}
+		marshalled_payload, _ := json.Marshal(payload)
+
+		req, err := http.NewRequest(http.MethodPost, "/register", bytes.NewReader(marshalled_payload))
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		resRecorder := httptest.NewRecorder()
+		router := mux.NewRouter()
+
+		router.HandleFunc("/register", handler.HandleRegister)
+		router.ServeHTTP(resRecorder, req)
+
+		if resRecorder.Code != http.StatusBadRequest {
+			t.Errorf("expected status code %d, got %d", http.StatusBadRequest, resRecorder.Code)
+		}
+	})
+}
+
+// * Implementing methods of interface "UserStore" in "mockUserStore".
+type mockUserStore struct {
+}
+
+func (m *mockUserStore) GetUserByEmail(email string) (*types.User, error) {
+	return nil, nil
+}
+func (m *mockUserStore) GetUserByID(id int) (*types.User, error) {
+	return nil, nil
+}
+func (m *mockUserStore) CreateUser(types.User) error {
+	return nil
+}
+
+//* END of implementation.

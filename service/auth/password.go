8e5c53c735c8cdb5a1da530bef315c3b8fc3e858
Winterson <u19cse1028@cit.ac.in>
2024-04-15 14:37:14 +0530

    Handling User Registration and Login

--- service/auth/password.go
+++ service/auth/password.go
@@ -12,3 +12,8 @@ func HashPassword(password string) (string, error) {
 
 	return string(hash), nil
 }
+
+func ComparePasswords(hashed string, plain []byte) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hashed), plain)
+	return err == nil
+}
--- service/user/routes.go
+++ service/user/routes.go
@@ -19,15 +19,38 @@ func NewHandler(store types.UserStore) *Handler {
 	return &Handler{store: store}
 }
 
+// * Route Registration
 func (handler *Handler) RegisterRoutes(router *mux.Router) {
 	router.HandleFunc("/login", handler.HandleLogin).Methods("POST")
 	router.HandleFunc("/register", handler.HandleRegister).Methods("POST")
 }
 
+// * Handling User Login
 func (handler *Handler) HandleLogin(res http.ResponseWriter, req *http.Request) {
+	var payload types.LoginUserPayload
+	if err := utils.ParseJSON(req, &payload); err != nil {
+		utils.WriteError(res, http.StatusBadRequest, err)
+		return
+	}
+	if err := utils.Validate.Struct(payload); err != nil {
+		errors := err.(validator.ValidationErrors)
+		utils.WriteError(res, http.StatusBadRequest, fmt.Errorf("invalid payload %v", errors))
+		return
+	}
 
+	user, err := handler.store.GetUserByEmail(payload.Email)
+	if err != nil {
+		utils.WriteError(res, http.StatusBadRequest, fmt.Errorf("not found, invalid email or password"))
+		return
+	}
+	if !auth.ComparePasswords(user.Password, []byte(payload.Password)) {
+		utils.WriteError(res, http.StatusBadRequest, fmt.Errorf("not found, invalid email or password"))
+	}
+
+	utils.WriteJSON(res, http.StatusOK, map[string]string{"token": ""})
 }
 
+// * Handling User Registration
 func (handler *Handler) HandleRegister(res http.ResponseWriter, req *http.Request) {
 
 	var payload types.RegisterUserPayload
